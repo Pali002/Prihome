0eb4099595a9e154c748ab0d986e27ae795502b7
Pali002 <zentaipali@gmail.com>
2023-02-19 12:14:20 +0100

    views, app kesz

--- desktop/src/views/MainView.java
+++ desktop/src/views/MainView.java
@@ -1,5 +1,24 @@
 package views;
 
+import java.util.ArrayList;
+
+import models.Employee;
+
 public class MainView {
     
+    public MainView() {
+
+    }
+
+    public void outEmployees(ArrayList<Employee> emps) {
+        for(Employee emp: emps) {
+            System.out.printf(
+                "|%15s|%15s|%15s|%15s|\n",
+                emp.getCity(),
+                emp.getAddress(),
+                emp.getSize(),
+                emp.getSalary()
+            );
+        }
+    }
 }
