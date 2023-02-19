5dd9ce96383b75290f51c6e7b3ea349d73f7ff04
Pali002 <zentaipali@gmail.com>
2023-02-19 12:05:01 +0100

    models

--- desktop/src/models/Restapi.java
+++ desktop/src/models/Restapi.java
@@ -1,5 +1,20 @@
 package models;
 
 public class Restapi {
+
+    public Restapi() {
+    
+    }
+
+    public String getEmployees() {
+        
+        HttpClient httpClient = new HttpClient();
+        String host = "http://[::1]:3000/";
+        String endpoint = "employees";
+        String urlStr = host + endpoint;
+        String res = httpClient.get(urlStr);
+
+        return res;
+    }
     
 }
